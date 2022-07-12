fe3473f40f6b23eaf777c9383c125ae0b88215dc
annnacaesar <Anechka22334@gmail.com>
2022-07-12 20:27:10 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { AppContainer, Container, TitleMain, Title } from './App.styled';
 
 export class App extends Component {
 	state = {
-		contacts: [
-			{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-			{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-			{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-			{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-		],
+		contacts: [],
 		filter: '',
 	};
 
@@ -45,6 +40,17 @@ export class App extends Component {
 
 		this.setState({ filter: value });
 	};
+
+	componentDidMount() {
+		const contacts = localStorage.getItem('contacts');
+		const parsedContacts = JSON.parse(contacts);
+
+		if (parsedContacts) this.setState({ contacts: parsedContacts });
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.state.contacts !== prevState.contacts) localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+	}
 
 	render() {
 		const { contacts, filter } = this.state;
